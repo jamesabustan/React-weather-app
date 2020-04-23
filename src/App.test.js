87ca253511267d54577d00abab7e85fd48e4dca1
jamesabustan <jamesabustan04@gmail.com>
2020-04-23 15:55:12 +0100

    fixed issue with npm test with watchman, enzyme installed - initial tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
-import WeatherTitle from '../src/components/header';
-import Form from '../src/components/form';
-import Weather from '../src/components/weather';
+import WeatherTitle from './components/weather';
+import Form from './components/form';
+import Weather from './components/weather';
+import Homepage from './container/Homepage';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+Enzyme.configure({ adapter: new Adapter() })
+
+test('Does App render homepage', () => {
+  const wrapper = mount(<App />)
+  expect(wrapper.find(".App").length).toEqual(1);
+});
+
+test('Does Homepage container load', () => {
+    const wrapper = shallow(<Homepage />)
+    expect(wrapper.find(".HomePage").length).toEqual(1);
+  });
+
+test('Does form component load', () => {
+    const wrapper = shallow(<Form />)
+    expect(wrapper.find(".formComponent").length).toEqual(1);
+  });
+  
+  test('Does header component load', () => {
+      const wrapper = mount(<WeatherTitle />)
+      expect(wrapper.find(".WeatherHeader").length).toEqual(1);
+      expect(wrapper.find(".animateText").length).toEqual(1);
+    });
+  
+  test('Does weather results load', () => {
+      const wrapper = mount(<Weather />)
+      expect(wrapper.find(".weatherComponent").length).toEqual(1);
+  });
